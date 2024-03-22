6f835e083f0ed93c412bc941f0e5b2cc0f1e8cff
breadNbutter42 <97899677+breadNbutter42@users.noreply.github.com>
2024-03-22 02:56:52 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/connectors/index.ts
@@ -0,0 +1,59 @@
+import { Web3Provider } from '@ethersproject/providers'
+import { InjectedConnector } from '@web3-react/injected-connector'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+import { WalletLinkConnector } from '@web3-react/walletlink-connector'
+import { PortisConnector } from '@web3-react/portis-connector'
+
+import { FortmaticConnector } from './Fortmatic'
+import { NetworkConnector } from './NetworkConnector'
+
+const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
+const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
+const PORTIS_ID = process.env.REACT_APP_PORTIS_ID
+
+export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '1')
+
+if (typeof NETWORK_URL === 'undefined') {
+  throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`)
+}
+
+export const network = new NetworkConnector({
+  urls: { [NETWORK_CHAIN_ID]: NETWORK_URL }
+})
+
+let networkLibrary: Web3Provider | undefined
+export function getNetworkLibrary(): Web3Provider {
+  return (networkLibrary = networkLibrary ?? new Web3Provider(network.provider as any))
+}
+
+export const injected = new InjectedConnector({
+  supportedChainIds: [43114, 43113]
+})
+
+// mainnet only
+export const walletconnect = new WalletConnectConnector({
+  rpc: { 1: NETWORK_URL },
+  bridge: 'https://bridge.walletconnect.org',
+  qrcode: true,
+  pollingInterval: 15000
+})
+
+// mainnet only
+export const fortmatic = new FortmaticConnector({
+  apiKey: FORMATIC_KEY ?? '',
+  chainId: 1
+})
+
+// mainnet only
+export const portis = new PortisConnector({
+  dAppId: PORTIS_ID ?? '',
+  networks: [1]
+})
+
+// mainnet only
+export const walletlink = new WalletLinkConnector({
+  url: NETWORK_URL,
+  appName: 'TraderQuest',
+  appLogoUrl:
+    'https://cdn.discordapp.com/attachments/1188170029573882016/1219187120208351323/zeemaaaaaaa__2acc0822-0f0c-48ce-acb4-c919262cb74d.webp?ex=660a637c&is=65f7ee7c&hm=1cfc69b1d3e6210309dbe7567b05ad310563f3eccb388be69432cefc3e9e378e&'
+})
---
+++ src/constants/lists.ts
@@ -0,0 +1,10 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/breadNbutter42/joe-tokenlists/main/mc.tokenlistDragon.json'
+//'tokens.uniswap.eth'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL
+  //,'https://raw.githubusercontent.com/breadNbutter42/joe-tokenlists/main/mc.tokenlist.json',
+ // 'tokens.1inch.eth', // 1inch
+//  'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
+]
